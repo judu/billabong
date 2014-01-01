f5cc452b720bcede3ce2d9235e7e9927ee3b507b
Julien Durillon <julien.durillon@gmail.com>
2014-01-01 22:46:16 +0100

    Go SVG. Make it playable.
    For now, we have:
     - A SVG board.
     - 2 players, not configurable for now.
     - We can put pions on the board
     - We can move pions according to the rules.
     - We can move a #1 color pion only if it's the #1 player's turn.

---
+++ game.js
@@ -0,0 +1,245 @@
+(function(){
+	function Vec(x,y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * Constructeur pour les objets de type Direction
+	 * (int,int,int)
+	 **/
+	function Direction(dx,dy,distance) {
+		this.dx = dx;
+		this.dy = dy;
+		this.distance = distance;
+	}
+
+	Direction.prototype.inc = function() {
+		return new Direction(this.dx,this.dy,this.distance +1);
+	}
+
+	if(!window.game) {
+		var cols = 16, rows = 14;
+
+		function Game(players) {
+			this.players = players;
+			this.current = 0;
+			this.pions = 0;
+			this.maxPionPerPlayer = 3;
+			this.cases = [];
+			for(var x = 0; x < cols; ++x) {
+				this.cases[x] = [];
+				for(var y = 0; y < rows; ++y) {
+					this.cases[x][y] = {};
+				}
+			}
+		}
+
+		Game.prototype.currentPlayer = function() {
+			return this.players[this.current];
+		}
+
+		Game.prototype.nextPlayer = function() {
+			this.current = (this.current + 1) % this.players.length;
+			return this.players[this.current];
+		}
+
+
+		/**
+		 * Fonction qui recherche le premier pion dans une direction.
+		 * Elle peut être récursive. L'idée est de rechercher pour chaque
+		 * direction (améliorer cet objet, il peut manquer des trucs) si la case
+		 * à la distance N est atteignable / est un pion. Si on a trouvé un
+		 * pion, on garde la direction (qu'on flag comme OK). Si on tombe sur un
+		 * empêchement (bord de terrain ou case du billabong), on supprime la
+		 * direction. Si on tombe sur une case vide, on garde la direction, en
+		 * mode NOK (ou pas trouvé) et on augmente sa distance.
+		 *
+		 *
+		 * Type : int -> int -> Direction -> Option[Direction]
+		 **/
+		Game.prototype.cherchePremierPion = function(vec,direction) {
+			var xtemp = vec.x + (direction.dx * direction.distance);
+			var ytemp = vec.y + (direction.dy * direction.distance);
+			if (isInterdite(xtemp, ytemp)){
+				//STOP
+				return undefined;
+			} else if (!this.isVide(xtemp,ytemp)) {
+				return direction;
+			} else {
+				return this.cherchePremierPion(vec, direction.inc());
+			}
+		}
+
+		function getAffine(orig,dest) {
+			var a = (dest.y - orig.y) / (dest.x - orig.x);
+			var b = orig.y - (a * orig.x);
+			return function(x) { return a*x + b;};
+		}
+
+		var riviereCoords = [];
+		for(var ry = 8; ry < rows; ++ry) {
+			riviereCoords.push(new Vec(7,ry));
+		}
+
+		function passeRiviere(orig,dest) {
+			var f = getAffine(orig,dest);
+			return _.some(riviereCoords, function(c) { return c.y === f(c.x);});
+		}
+
+		function scoreDeplacement(orig,dest) {
+			if((orig.x <= 7 && dest.x > 7) || (orig.x > 7 && dest.x <= 7)) {
+				if(passeRiviere(orig,dest)) {
+					if(orig.x <= 7) {
+						dest.score = (orig.score ? orig.score : 0) + 1;
+					} else {
+						dest.score = (orig.score ? orig.score : 0) - 1;
+					}
+				} else dest.score = (orig.score ? orig.score : 0);
+			} else dest.score = (orig.score ? orig.score : 0);
+			return dest;
+		}
+
+		/**
+		 * À partir de coordonnées, trouver toutes les possibilités pour un
+		 * saut. On a les cellules (un tableau) et les coordonnées de tous les
+		 * pions (peut-être avec celles des cases du billabong)
+		 *
+		 * Type : ioVec -> iio.Grid -> [ioVec]
+		 **/
+		Game.prototype.getSingleJumpDestinations = function(coordPion) {
+			var self = this;
+			return _.reduce(_.reduce(initialiseDirections(),function(t,d) {
+				var p = self.cherchePremierPion(coordPion,d);
+				if(p)
+				t.push(p)
+				return t;
+			},[]),function(t,d) {
+				var pp = self.isSautPossible(coordPion,d);
+				if(pp) {
+					t.push(scoreDeplacement(coordPion,pp));
+				}
+				return t;
+			},[]);
+		}
+
+		/**
+		 *	À partir d'un tableau de directions qui indiquent des pions, il faut
+		 *	tester si on peut sauter par dessus le pion. En gros, il faut suivre
+		 *	le chemin de la `direction` après le pion pour voir s'il y a assez de
+		 *	place pour sauter et s'il n'y a pas un autre pion sur le chemin
+		 *
+		 *	L'idée si possible serait de ressortir une liste de coordonnées
+		 *	atteignables par un saut.
+		 *
+		 *	Type : ioVec -> Direction -> [ioVec]
+		 */
+		Game.prototype.isSautPossible = function(vec,pionASauter) {
+			var xpion = vec.x + (pionASauter.dx * pionASauter.distance);
+			var ypion = vec.y + (pionASauter.dy * pionASauter.distance);
+			for(var a = 1; a <= pionASauter.distance; ++a) {
+				var xtemp = xpion + (pionASauter.dx * a);
+				var ytemp = ypion + (pionASauter.dy * a);
+				if (isInterdite(xtemp, ytemp) || !(this.isVide(xtemp,ytemp))){
+					//STOP
+					return undefined;
+				}
+			}
+			return {x:xtemp,y:ytemp};
+		}
+
+		function uniqCoords(coords) {
+			return _.uniq(coords, function(co) {
+				var xstr = (co.x < 10 ? '0': '') + co.x;
+				var ystr = (co.y < 10 ? '0': '') + co.y;
+				return xstr + ystr;
+			});
+		}
+
+		Game.prototype.getAllJumpDestinations = function(coordPion) {
+			var self = this;
+			var lastLength = 0;
+			var res = this.getSingleJumpDestinations(coordPion);
+			var res2 = _.clone(res);
+			while(res.length > lastLength) {
+				lastLength = res.length;
+				res2 = uniqCoords(_.flatten(_.map(res2, function(co){return self.getSingleJumpDestinations(co);})));
+				res = uniqCoords(res.concat(res2));
+			}
+			res = uniqCoords(_.map(_.filter(_.map(initialiseDirections(),function(direction) {
+					return new Vec(coordPion.x + (direction.dx * direction.distance),coordPion.y + (direction.dy * direction.distance));
+				}), function(coords) {
+					return !isInterdite(coords.x,coords.y) && self.isVide(coords.x,coords.y);
+				}), function(coords) {
+					return scoreDeplacement(coordPion,coords);
+				}).concat(res));
+			if(coordPion.passed) {
+				res = _.reject(res, function(c) { return c.score < 0;});
+			}
+			return res;
+		}
+
+		// Initialise the board. All squares are empty.
+		function initialiseDirections() {
+			var dirs = [];
+			for(var x = -1; x <= 1; ++x) {
+				for(var y = -1; y <= 1; ++y) {
+					if(x !== 0 || y !== 0) dirs.push(new Direction(x,y,1));
+				}
+			}
+			return dirs;
+		}
+		/**
+		 * Fonction qui vérifie si une case est utilisable ou non.
+		 * Renvoie vrai si on dépasse le bord ou si on est dans l'eau.
+		 *
+		 * Type : int -> int -> bool
+		 **/
+		function isInterdite(x,y){
+			return !(_.isNaN(x) || _.isNaN(y)) && ((x<0 || y<0 || x>=cols || y>=rows) || (x>=6 && x<=9 && y>=6 && y<=7));
+		}
+
+		/**
+		 * Fonction qui vérifie si une case est occupée ou non.
+		 * Renvoie vrai si la case est vide.
+		 *
+		 * Type : int -> int -> bool
+		 **/
+		Game.prototype.isVide = function(x,y){
+			return typeof(this.cases[x][y].pion) === 'undefined';
+		}
+
+		Game.prototype.stillSetUp = function() {
+			return this.pions < this.maxPionPerPlayer * this.players.length;
+		}
+
+		Game.prototype.setPion = function(x,y,couleur) {
+			this.cases[x][y].pion = couleur;
+		}
+
+		Game.prototype.getPion = function(x,y) {
+			return this.cases[x][y].pion;
+		}
+		Game.prototype.removePion = function(x,y) {
+			this.cases[x][y].pion = undefined;
+		}
+
+		Game.prototype.addPion = function(x,y) {
+			if(this.isVide(x,y)) {
+				this.setPion(x,y,this.currentPlayer());
+				this.nextPlayer();
+				++this.pions;
+				return this.getPion(x,y);
+			} else return false;
+		}
+		Game.prototype.move = function(orig,dest) {
+			if(this.isVide(dest.x,dest.y)) {
+				this.setPion(dest.x,dest.y,this.getPion(orig.x,orig.y));
+				this.removePion(orig.x,orig.y);
+				this.nextPlayer();
+				return true;
+			} else return false;
+		}
+		window.Game = Game;
+	}
+})();
---
+++ plateau.d3.js
@@ -0,0 +1,198 @@
+(function(){
+
+	d3.plateau = function() {
+		// 16 cols from 0 to 15.
+		var game = new window.Game();
+		var cols = 16, rows = 14, cellSize = 50, strokeWidth=2;
+		var minMareX = 6, maxMareX = 9;
+		var minMareY = 6, maxMareY = 7;
+		var vertSepRange = d3.range(2,(cols+1)*cellSize+2,cellSize);
+		var horizSepRange = d3.range(2,(rows+1)*cellSize+2,cellSize);
+		var svg = null; // Main svg markup
+		var squares = null; // svg group containing the squares
+		var pionGroup = null; // svg group containing the pions
+
+		var squareCoords = [];
+		for(var x = 0; x < cols; ++x) {
+			for(var y = 0; y < rows; ++y) {
+				squareCoords.push({x:x,y:y});
+			}
+		}
+		squareCoords = squareCoords.filter(function(s) {return !(s.x >= minMareX && s.x <= maxMareX && s.y >= minMareY && s.y <= maxMareY);});
+
+		function centerOf(vec) {
+			vec.cx = vec.x * cellSize + strokeWidth + (cellSize/2);
+			vec.cy = vec.y * cellSize + strokeWidth + (cellSize/2);
+			return vec;
+		}
+
+		function addPion(obj,pion) {
+			obj.pion = pion;
+			return obj;
+		}
+
+		function movePion(destDat,pionDat,pionElm) {
+			// "this" is the clicked square
+			if(game.move(pionDat,destDat)){
+				if(destDat.score > 0) //TODO: check that pion is already passed to let it win.
+					destDat.passed = true;
+				else
+					destDat.passed = pionDat.passed;
+				destDat.score = 0;
+				destDat.pion = pionDat.pion;
+				d3.select(pionElm).datum(destDat)
+					.attr('cx', function(d){return d.cx})
+					.attr('cy', function(d){return d.cy});
+			}
+			unhighlight();
+		}
+
+		function accessibleClickCallback(dest, pionDat, elm) {
+			return function(d) {movePion.call(this, dest, pionDat, elm);};
+		}
+
+		function displayAccessible(datum, index) {
+			// Display possibilities if we clicked a pion.
+			if(!game.stillSetUp()) {
+				if(game.currentPlayer() === datum.pion) {
+					var self = this;
+					unhighlight();
+					d3.select(self).classed('active',true).attr('filter', 'url(#shadow)');
+					var dests = _.map(game.getAllJumpDestinations(datum),function(d) {return centerOf(d);});
+					for(var i = 0; i < dests.length; ++i) {
+						var dest = dests[i];
+						squares
+							.selectAll('rect.square')
+							.filter(function(d) {return d.x === dest.x && d.y === dest.y;})
+							.classed('accessible',true)
+							.on('click',accessibleClickCallback(dest,datum,self));
+					}
+				} else console && console.log('This is not your pion');
+			} else console && console.log('Game is in set up phase');
+		}
+
+		function unhighlight() {
+			pionGroup.select('circle.pion.active').attr('filter',null).classed('active',false);
+			squares.selectAll('rect.square').classed('accessible',false).on('click',unhighlight);
+		}
+
+		function squareClicked(datum, index) {
+			if(game.stillSetUp()) {
+				var pion = game.addPion(datum.x,datum.y);
+				if(pion) {
+					pionGroup = pionGroup || svg.append('g').attr('id', 'piongroup');
+					pionGroup.append('svg:circle').data([addPion(centerOf(datum),pion)])
+						.attr('cx', function(d){return d.cx})
+						.attr('cy', function(d){return d.cy})
+						.attr('r', 15)
+						.attr('class', 'pion')
+						.style('fill', pion)
+						.on('click',displayAccessible);
+				}
+				if(!game.stillSetUp()) {
+					squares.selectAll('rect.square').on('click',unhighlight);
+				}
+			}
+		}
+
+		// Draw the svg plateau in `elem`.
+		function plateau(elem) {
+			elem.each(function() {
+				var g = d3.select(this);
+				svg = svg || g.append('svg');
+				drawCases(svg);
+			});
+			return plateau;
+		}
+
+		function drawCases(g) {
+			g.attr('width', cols*cellSize+10).attr('height', rows*cellSize+10);
+			// Create the filter to emphasize the selected pions.
+			var defs = g.append('svg:defs');
+			var filter = defs.append('svg:filter').attr('id','shadow')
+				.attr('primitiveUnits','objectBoundingBox');
+			filter.append('svg:feSpecularLighting')
+				.attr('surfaceScale',1)
+			   .attr('lighting-color', '#000000')
+				.attr('in','SourceAlpha')
+				.attr('result','specOut')
+			.append('svg:fePointLight')
+			   .attr('x','5')
+			   .attr('y','5')
+			   .attr('z','25');
+			filter.append('svg:feComposite')
+				.attr('in2','specOut')
+				.attr('in','SourceGraphic')
+				.attr('operator','arithmetic')
+				.attr('k1',1)
+				.attr('k2',0.5)
+				.attr('k3',0)
+				.attr('k4',0);
+			 //Filter created.
+			var lines = g.append('svg:g').attr('id', 'thelines');
+			lines.selectAll('line.vertical')
+				.data(vertSepRange).enter().append('svg:line')
+				.attr('x1', function(d) { return d;})
+				.attr('y1', strokeWidth)
+				.attr('x2', function(d) { return d;})
+				.attr('y2', rows*cellSize+strokeWidth)
+				.attr('class', 'separateur');
+			lines.selectAll('line.horizontal')
+				.data(horizSepRange)
+				.enter().append('svg:line')
+				.attr('x1', strokeWidth)
+				.attr('y1', function(d) { return d;})
+				.attr('x2', cols*cellSize+strokeWidth)
+				.attr('y2', function(d) { return d;})
+				.attr('class', 'separateur');
+			squares = squares || g.append('g').attr('id','squares');
+			squares.selectAll('rect.cases')
+				.data(squareCoords)
+				.enter().append('svg:rect')
+				.attr('x',function(d){return d.x*cellSize+3})
+				.attr('y',function(d){return d.y*cellSize+3})
+				.attr('data-x', function(d){return d.x})
+				.attr('data-y', function(d){return d.y})
+				.attr('width',cellSize-strokeWidth)
+				.attr('height', cellSize-strokeWidth)
+				.classed('obscur', function(d) {return ((d.x % 2 === 0 && d.y % 2 === 1) || (d.x % 2 === 1 && d.y % 2 === 0))})
+				.classed('clair', function(d) {return !((d.x % 2 === 0 && d.y % 2 === 1) || (d.x % 2 === 1 && d.y % 2 === 0))})
+				.classed('square', true)
+				.on('click',squareClicked);
+			g.append('svg:rect')
+				.attr('id','pond')
+				.attr('x', minMareX*cellSize+3)
+				.attr('y', minMareY*cellSize+3)
+				.attr('width', (maxMareX-minMareX+1)*cellSize-strokeWidth)
+				.attr('height', (maxMareY-minMareY+1)*cellSize-strokeWidth)
+				.style('fill', '#000099');
+			g.append('svg:line')
+				.attr('id', 'river')
+				.attr('x1', (cols/2) * cellSize + 3)
+				.attr('y1', (maxMareY+1)*cellSize)
+				.attr('x2', (cols/2) * cellSize+3)
+				.attr('y2', rows*cellSize+strokeWidth)
+				.attr('class', 'river');
+
+			return plateau;
+		}
+
+		plateau.players = function(_) {
+			if(!arguments.length) return game.players;
+			game.players = _;
+			return plateau;
+		}
+
+		plateau.addPlayer = function(p) {
+			if(!d3.set(game.players).has(p))
+				game.players.push(p);
+			return plateau;
+		}
+
+		plateau.game = function() {
+			return game;
+		}
+
+		return plateau;
+	};
+})();
